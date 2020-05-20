f10cc535f01b65a51cf68febefd4ab1b1c7cbf8a
tenma.yamazaki <tenma.yamazaki@ari-jp.com>
2020-05-20 20:54:23 +0900

    キャッシュテスト

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -4,15 +4,15 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 // ファイルのキャッシュ
 workbox.precaching.precacheAndRoute([
     {
-        url:'/index.html',
+        url:'./index.html',
         revision:'12345'
     },
     {
-        url:'/script/acc.js',
+        url:'./script/acc.js',
         revision:'12345'
     },
     {
-        url:'/style/acc.css',
+        url:'./style/acc.css',
         revision:'12345'
     }
 ]);
