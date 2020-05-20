55afe3aa7b160af38b28ec249b29a6e4a68e9c73
tenma.yamazaki <tenma.yamazaki@ari-jp.com>
2020-05-20 20:26:04 +0900

    キャッシュ機能追加

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -1,3 +1,22 @@
+// ライブラリのインポート
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+// ファイルのキャッシュ
+workbox.precaching.precacheAndRoute([
+    {
+        url:'/index.html',
+        revision:'12345'
+    },
+    {
+        url:'/script/acc.js',
+        revision:'12345'
+    },
+    {
+        url:'/style/acc.css',
+        revision:'12345'
+    }
+]);
+
 self.addEventListener('fetch', function(e) {
 
 });
